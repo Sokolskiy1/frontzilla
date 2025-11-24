b5497bfb39cf6842df42f47b3319f5f7ba194677
sokolskiy <vanosocol1@gmail.com>
2025-11-24 03:31:41 +0300

    add class Tube and helper function(on create mass)

--- src/sorting_liquids/main_sort.js
+++ src/sorting_liquids/main_sort.js
@@ -1,3 +1,29 @@
+//Двумерный массив не очень подходящая структура для решения задачи
+//Сделаем массив объектов класса tube для структурирования
+
+class Tube {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.contents = [];
+    }
+    push(color) {
+        this.contents.push(color);
+    }
+}
+
+function createTubesFromArray(tubesArray) {
+
+    return tubesArray.map(tubeArr => {
+        const tube = new Tube(tubesArray[0].length);
+        tubeArr
+            .filter(color => color !== null )
+            .forEach(color => tube.push(color));
+
+        return tube;
+    });
+
+
+}
 
 function main() {
 
@@ -16,8 +42,9 @@ function main() {
     initialTubesArray.forEach((tube, i) => {
         console.log(` ${i}: [${tube.join(', ')}]`);
     });
-
-
+    const tubesClass = createTubesFromArray(initialTubesArray);
+    console.log("\n Массив туб:\n");
+    console.log(tubesClass)
 }
 
 
