b9f4c0e7aa6f166218da6475c55afb3e4ef1b719
sokolskiy <vanosocol1@gmail.com>
2025-11-24 04:16:44 +0300

    class image and function decision

--- src/sorting_liquids/main_sort.js
+++ src/sorting_liquids/main_sort.js
@@ -1,6 +1,63 @@
 //Двумерный массив не очень подходящая структура для решения задачи
 //Сделаем массив объектов класса tube для структурирования
 
+class ImageState{
+    constructor(tubes) {
+        this.initialTubes = tubes.map(tube => tube.clone());
+        this.move = null;
+        this.parent = null;
+    }
+
+
+    clone() {
+        return new ImageState(this.initialTubes);
+    }
+
+    isWin() {
+        return this.initialTubes.map(tube => {
+            if (tube.isEmpty()) return true;
+
+            const firstColor = tube.contents[0];
+            return tube.contents.every(color => color === firstColor);
+        }).every(result => result === true);
+    }
+
+    getPath() {
+        const path = [];
+        let current = this;
+
+        while (current.move !== null) {
+            path.unshift(current.move);
+            current = current.parent;
+        }
+
+        return path;
+    }
+
+
+
+}
+
+
+function  breadth_traversal(initialTubes){
+    const startState = new  ImageState(initialTubes);
+    const queue = [startState];
+    const visited = new Set([]);
+    let count = 0
+
+    while (queue.length > 0) {
+        const currentState = queue.shift();
+        console.log(currentState)
+        count += 1;
+        if (currentState.isWin()) {
+            console.log(`Решение за: ${count} `);
+            return currentState.getPath();
+        }
+
+    }
+}
+
+
 class Tube {
     constructor(capacity) {
         this.capacity = capacity;
@@ -8,6 +65,14 @@ class Tube {
     }
     push(color) {
         this.contents.push(color);
+    }
+    clone() {
+        const newTube = new Tube(this.capacity);
+        newTube.contents = [...this.contents];
+        return newTube;
+    }
+    isEmpty() {
+        return this.contents.length === 0;
     }
 }
 
@@ -45,6 +110,7 @@ function main() {
     const tubesClass = createTubesFromArray(initialTubesArray);
     console.log("\n Массив туб:\n");
     console.log(tubesClass)
+    console.log(breadth_traversal(tubesClass))
 }
 
 
